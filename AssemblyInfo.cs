622faf9dd8becc035f381a8124ec34c4937ea06e
Tonypadgett <157521742+Tonypadgett@users.noreply.github.com>
2025-05-08 21:37:50 -0400

    AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using MelonLoader;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: MelonInfo(typeof(OVRPlugin), "Bsu's Temp", "Skibidi", "Dont skid or i will find you", "")]
+[assembly: MelonGame(null, null)]
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("18868-13842")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 18868-13842 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ca8c2ec1-a3f9-4480-bd57-ed61addb5e25")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
